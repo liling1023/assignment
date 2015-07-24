979414c2dc428c3b39b5cb4c5a92ccb4c108a416
liling1023 <986840540@qq.com>
2015-07-24 17:38:49 +0800

    modified OutpuMaxWeight

--- 7.24test.2/OutputMaxWeight.java
+++ 7.24test.2/OutputMaxWeight.java
@@ -7,51 +7,66 @@ import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class OutputMaxWeight {
-	public static String maxWeight(StringBuffer buffer) {
-		
-		 Pattern expression = Pattern.compile("[a-zA-Z]+");// 定义正则表达式匹配单词
-	        String string = buffer.toString();
-	        Matcher matcher = expression.matcher(string);//
-	        Map<String, Integer> map = new TreeMap<String, Integer>();
-	        String word = "";
-	        int times = 0;
-	        while (matcher.find()) {
-	            word = matcher.group();
-	            if (map.containsKey(word)) {
-	                times = map.get(word);
-	                map.put(word, times + 1);
-	            } else {
-	                map.put(word, 1);
-	            }
-	        }
-	        List<Map.Entry<String, Integer>> list = new ArrayList<Map.Entry<String, Integer>>(
-	                map.entrySet());
-	        Comparator<Map.Entry<String, Integer>> comparator = new Comparator<Map.Entry<String, Integer>>() {
-	            public int compare(Map.Entry<String, Integer> left,
-	                    Map.Entry<String, Integer> right) {
-	                return (left.getValue()).compareTo(right.getValue());
-	            }
-	        };
-	        Collections.sort(list, comparator);
-	        int last = list.size() - 1;
-	        for (int i = last; i > last - 10; i--) {
-	            String key = list.get(i).getKey();
-	            Integer value = list.get(i).getValue();
-	            System.out.println(key + " :" + value);
-	        }
-			return word;
+	public static void maxWeight(StringBuffer buffer) {
+		Pattern p = Pattern.compile("[a-zA-Z]+");
+		String string = buffer.toString();
+		Matcher matcher = p.matcher(string);
+		Map<String, Double> map = new HashMap<String, Double>();
+		String word = "";
+		Double times = 0.0;
+		while (matcher.find()) {
+			word = matcher.group();
+			if (map.containsKey(word)) {
+				times = map.get(word);
+				map.put(word, times + 1);
+			} else {
+				map.put(word,  1.0);
+			}
+		}
+		System.out.println("before"+map);
+		String[] s = { "am", "is", "are", "has", "been", "has", "have", "had",
+				"a", "an", "the", "in", "at", "on", "to", "or" };
+		int weight=0;
+		Set set=map.entrySet();
+        Iterator it=set.iterator();
+        while(it.hasNext()){
+            Map.Entry me=(Map.Entry)it.next();
+            String key = (String) me.getKey();
+			Double value = (Double) me.getValue();
+    			for (int j = 0; j < s.length; j++) {
+    				if (key.equals(s[j]))
+    					value =(Double)(0.2*value);
+    				else
+    					value=1*value;
+    				map.put(key,value);
+    			}
+        }
+    			System.out.println("change:"+map);
+    			List<Map.Entry<String, Double>> list = new ArrayList<Map.Entry<String, Double>>(
+    					map.entrySet());
+    			Comparator<Map.Entry<String, Double>> comparator = new Comparator<Map.Entry<String, Double>>() {
+    				public int compare(Map.Entry<String, Double> left,
+    						Map.Entry<String, Double> right) {
+    					return (left.getValue()).compareTo(right.getValue());
+    				}
+    			};
+    			Collections.sort(list, comparator);
+    			int max=list.size()-1;
+    			for (int i =max ; i >max-10; i--) {
+    			    String id = list.get(i).toString();
+    			    System.out.println(id);
+    			}
 	}
-
 	public static void main(String[] args) throws IOException {
 		String fileName1 = "C:\\Users\\TEMP.114-20150325VTZ.006\\Desktop\\src.txt";
 		FileReader fr = new FileReader(fileName1);
 		BufferedReader reader = new BufferedReader(fr);
-		 StringBuffer buffer = new StringBuffer();
+		StringBuffer buffer = new StringBuffer();
 		String line = null;
 		while ((line = reader.readLine()) != null) {
-			 buffer.append(line);
+			buffer.append(line); 
 		}
 		reader.close();
-		System.out.println(maxWeight(buffer));
+		maxWeight(buffer);
 	}
 }
